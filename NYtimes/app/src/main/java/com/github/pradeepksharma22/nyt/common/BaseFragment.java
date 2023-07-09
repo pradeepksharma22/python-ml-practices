a0f4ffeb920139de4b13369fd36fc3a029a02e51
Pradeep Kumar Sharma <42090973+pradeepksharma22@users.noreply.github.com>
2023-07-09 19:09:20 +0530

    Delete NYtimes directory

--- NYtimes/app/src/main/java/com/github/pradeepksharma22/nyt/common/BaseFragment.java
+++
@@ -1,6 +0,0 @@
-package com.github.pradeepksharma22.nyt.common;
-
-import android.support.v4.app.Fragment;
-
-public class BaseFragment extends Fragment {
-}
--- NYtimes/app/src/main/java/com/github/pradeepksharma22/nyt/net/HttpRequest.java
+++
@@ -1,9 +0,0 @@
-package com.github.pradeepksharma22.nyt.net;
-
-import com.github.pradeepksharma22.nyt.Constants;
-
-public interface HttpRequest {
-    String API_KEY = Constants.API_KEY;
-    String BASE_URL = Constants.BASE_URL;
-    void execute(HttpResponseListener listener);
-}
